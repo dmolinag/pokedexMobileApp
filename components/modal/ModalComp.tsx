6882b6c25a1f1cdc41c88b6e636d749ff8bef421
Daniel Molina <daniel.molina3192@gmail.com>
2024-03-07 14:27:20 -0500

    pokemon info modal

---
+++ components/modal/ModalComp.tsx
@@ -0,0 +1,53 @@
+import { StyleSheet, View } from "react-native";
+import { Modal } from "react-native";
+
+export interface ModalCompProps {
+  children: React.ReactElement;
+  isVisible?: boolean;
+}
+
+export const ModalComp = ({ children, isVisible }: ModalCompProps) => {
+  return (
+    <Modal visible={isVisible} transparent>
+      <View style={styles.container}>
+        <View style={styles.modalView}>{children}</View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 10,
+    backgroundColor: "rgba(0,0,0,0.5)",
+    width: "80%",
+    borderRadius: 20,
+    padding: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  input: {
+    backgroundColor: "#fff",
+    borderWidth: 1,
+    padding: 10,
+    width: "60%",
+    borderRadius: 10,
+    color: "black",
+  },
+  loading: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- containers/mainPage/pokemonsList/PokemonsList.tsx
+++ containers/mainPage/pokemonsList/PokemonsList.tsx
@@ -1,6 +1,17 @@
 import { useState } from "react";
-import { StyleSheet, View, Text, Image, Platform } from "react-native";
-import { useListPokemon, useListPokemonByType } from "../../../customHooks";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Platform,
+  Pressable,
+} from "react-native";
+import {
+  INITIAL_POKEMON,
+  useListPokemon,
+  useListPokemonByType,
+} from "../../../customHooks";
 import {
   POKEMONS_PER_PAGE,
   PokemonObj,
@@ -9,19 +20,20 @@ import {
   usePokemonsListContext,
 } from "../../../utils";
 import { Button, Card, SearchBar } from "../../../components";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { ModalComp } from "../../../components/modal";
 
 export const PokemonList = () => {
-  const { queryPokemons } = useListPokemon();
   const { pokemonList, filtered, pokemonType } = usePokemonsListContext();
-
+  const { queryPokemons } = useListPokemon();
   const { queryPokemonsByType } = useListPokemonByType();
 
-  // const [isPokemonDetailsModalOpen, setIsPokemonDetailsModalOpen] =
-  // 	useState<boolean>(false);
+  const [isPokemoninfoModalOpen, setIsPokemonInfoModalOpen] =
+    useState<boolean>(false);
   // const [isPokemonTypeModalOpen, setIsPokemonTypeModalOpen] =
   // 	useState<boolean>(false);
-  // const [selectedPokemon, setSelectedPokemon] =
-  // 	useState<PokemonObj>(INITIAL_POKEMON);
+  const [selectedPokemon, setSelectedPokemon] =
+    useState<PokemonObj>(INITIAL_POKEMON);
   // const [favPokemons, setFavPokemons] = useState<number[]>(getFavoritePokemons);
   // const [pokemonTypeAgainst, setPokemonTypeAgainst] = useState<string>('');
   const [page, setPage] = useState<number>(1);
@@ -37,10 +49,52 @@ export const PokemonList = () => {
     setPage(nextPage);
   };
 
+  const handleOpenModal = (pokemon: PokemonObj) => {
+    setIsPokemonInfoModalOpen(true);
+    setSelectedPokemon(pokemon);
+  };
+
   return (
     <View style={styles.container}>
+      <View>
+        <ModalComp isVisible={isPokemoninfoModalOpen}>
+          <View style={styles.content}>
+            <View>
+              <Text style={styles.number}>{selectedPokemon.name}</Text>
+            </View>
+            <View style={styles.stats}>
+              {selectedPokemon.stats.map((item) => {
+                const pokemonStat =
+                  item.stat.name.charAt(0).toUpperCase() +
+                  item.stat.name.slice(1);
+
+                return (
+                  <View style={styles.meterContainer} key={item.stat.name}>
+                    <Text style={styles.text}>{pokemonStat}</Text>
+                    <Text style={styles.text}>{item.base_stat}</Text>
+                    {/* <Meter value={item.base_stat} aria-label={item.stat.name}>
+                      {({ percentage }) => (
+                        <div className="bar">
+                          <div
+                            className={`fill ${styles.meter}`}
+                            style={{ width: percentage + "%" }}
+                          />
+                        </div>
+                      )}
+                    </Meter> */}
+                  </View>
+                );
+              })}
+            </View>
+            <Button onPress={() => setIsPokemonInfoModalOpen(false)}>
+              <Text>close</Text>
+            </Button>
+          </View>
+        </ModalComp>
+      </View>
+
       <SearchBar setPage={setPage} />
-      <View style={styles.pokemons}>
+      <View style={styles.pokemonsList}>
         {pokemonList.map((pokemon: PokemonObj) => {
           const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`;
 
@@ -52,9 +106,34 @@ export const PokemonList = () => {
               >
                 <View style={styles.pokemon}>
                   <View style={styles.pokemonInfo}>
-                    <Text style={styles.number}>
-                      {formatPokemonId(pokemon.id)}
-                    </Text>
+                    <View style={styles.pokemonInfoHeader}>
+                      <Text style={styles.number}>
+                        {formatPokemonId(pokemon.id)}
+                      </Text>
+
+                      <View style={styles.iconsContainer}>
+                        <Icon
+                          style={styles.icon}
+                          name="info-circle"
+                          size={30}
+                          color="grey"
+                          onPress={() => handleOpenModal(pokemon)}
+                        />
+                        <Icon
+                          style={styles.icon}
+                          name="heart"
+                          size={30}
+                          color="#900"
+                        />
+                        <Icon
+                          style={styles.icon}
+                          name="heart-o"
+                          size={30}
+                          color="#900"
+                        />
+                      </View>
+                    </View>
+
                     <Text style={styles.number}>{pokemon.name}</Text>
                   </View>
 
@@ -72,7 +151,6 @@ export const PokemonList = () => {
           );
         })}
       </View>
-
       <Button onPress={handleLoadMore}>Load more</Button>
     </View>
   );
@@ -82,12 +160,29 @@ const styles = StyleSheet.create({
   container: {
     display: "flex",
     flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
     width: "100%",
     padding: 20,
     gap: 50,
   },
-  pokemons: {
+  content: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  stats: {},
+  meterContainer: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 5,
+    width: "100%",
+    justifyContent: 'space-between'
+  },
+  text: {
+    color: "#fff",
+  },
+  pokemonsList: {
     display: "flex",
     flexDirection: "column",
     gap: 20,
@@ -102,6 +197,13 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+    width: "100%",
+  },
+  pokemonInfoHeader: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "center",
   },
   number: {
     color: "white",
@@ -111,6 +213,22 @@ const styles = StyleSheet.create({
     textShadowOffset: { width: 2, height: 2 },
     textShadowRadius: 4,
     textTransform: "capitalize",
+  },
+  iconsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    alignSelf: "flex-end",
+    alignContent: "flex-end",
+    position: "absolute",
+    right: 0,
+    borderRightColor: "black",
+    borderWidth: 1,
+  },
+  icon: {
+    display: "flex",
+    alignSelf: "flex-end",
   },
   imageContainer: {
     width: "100%",
